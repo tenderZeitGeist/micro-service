658f36aa1002b7f38e00833a1c3a558b0d63edbb
Zeitgeist <zeit_geist@posteo.de>
2024-04-11 22:54:35 +0200

    Preliminary layout for routes

--- main.cpp
+++ main.cpp
@@ -3,23 +3,43 @@
 #include <rest/Alias.hpp>
 #include <rest/Controller.hpp>
 #include <rest/Connection.hpp>
-#include <rest/Route.hpp>
+
+constexpr std::size_t kMaxThreads = 4;
 
 int main() {
-    auto address = net::ip::make_address(rest::kDefaultAddress);
-    const auto numOfThreads = std::thread::hardware_concurrency();
-    rest::Connection connection(address, rest::kDefaultPort, numOfThreads);
+    const auto numOfThreads = std::min(static_cast<std::size_t>(std::thread::hardware_concurrency()), kMaxThreads);
+    rest::Connection connection(net::ip::make_address(rest::kDefaultAddress), rest::kDefaultPort, numOfThreads);
     const auto controller = connection.initializeController({
         {
             rest::Method::get,
-            rest::Endpoint("/compound"),
+            rest::Endpoint("/compounds"),
             [](const rest::HttpRequest&) { return rest::Response(rest::http::status::ok, "Foo"); }
         },
         {
             rest::Method::get,
-            rest::Endpoint("/zoo"),
+            rest::Endpoint("/compounds/{name}"),
             [](const rest::HttpRequest&) { return rest::Response(rest::http::status::ok, "Bar"); }
-        }
+        },
+        {
+            rest::Method::get,
+            rest::Endpoint("/animals/{name}"),
+            [](const rest::HttpRequest&) { return rest::Response(rest::http::status::ok, "Bar"); }
+        },
+        {
+            rest::Method::post,
+            rest::Endpoint("compounds/{name}/animal/{name,species,age}"),
+            [](const rest::HttpRequest&) { return rest::Response(rest::http::status::ok, "Bar"); }
+        },
+        {
+            rest::Method::delete_,
+            rest::Endpoint("compounds/{name}/animal/{name}"),
+            [](const rest::HttpRequest&) { return rest::Response(rest::http::status::ok, "Bar"); }
+        },
+        {
+            rest::Method::get,
+            rest::Endpoint("/animal/{species}"),
+            [](const rest::HttpRequest&) { return rest::Response(rest::http::status::ok, "Bar"); }
+        },
     });
     connection.run();
     return 0;
